ec4f9eb3f35908d8714a568d7a31c3d209e9419c
unknown <sushan.thapa1010@gmail.com>
2024-02-12 08:33:21 -0500

    web api created and class library added

---
+++ CalcLibrary/CalculatorLogicService.cs
@@ -0,0 +1,32 @@
+﻿namespace CalcLibrary
+{
+    public class CalculatorLogicService
+    {
+        public decimal Add(decimal number1, decimal number2)
+        {
+            decimal addResult  = number1+ number2;
+            return addResult;
+        }
+
+        public decimal Subtract(decimal number1, decimal number2)
+        {
+            decimal subractResult = number1 - number2;
+            return subractResult;
+        }
+        public decimal Multiply(decimal number1, decimal number2)
+        {
+            decimal MulResult = number1 * number2;
+            return MulResult;
+        }
+        public decimal Divide(decimal number1, decimal number2)
+        {
+            decimal DivResult = number1 / number2;
+            return DivResult;
+        }
+        public decimal Modulo(decimal number1, decimal number2)
+        {
+            decimal ModResult = number1 % number2;
+            return ModResult;
+        }
+    }
+}
---
+++ CalculatorAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,47 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using CalcLibrary; // Adjust this based on your actual namespace
+
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class CalculatorController : ControllerBase
+{
+    private readonly CalculatorLogicService _calculatorService;
+
+    public CalculatorController(CalculatorLogicService calculatorService)
+    {
+        _calculatorService = calculatorService;
+    }
+    [HttpGet("add")]
+    public decimal Add(decimal number1, decimal number2)
+    {
+        return _calculatorService.Add(number1, number2);
+
+    }
+
+    [HttpGet("subtract")]
+    public decimal Subtract(decimal number1, decimal number2)
+    {
+        decimal subractResult = number1 - number2;
+        return subractResult;
+    }
+    [HttpGet("multiply")]
+    public decimal Multiply(decimal number1, decimal number2)
+    {
+        decimal MulResult = number1 * number2;
+        return MulResult;
+    }
+    [HttpGet("divide")]
+    public decimal Divide(decimal number1, decimal number2)
+    {
+        decimal DivResult = number1 / number2;
+        return DivResult;
+    }
+    [HttpGet("modulo")]
+    public decimal Modulo(decimal number1, decimal number2)
+    {
+        decimal ModResult = number1 % number2;
+        return ModResult;
+    }
+}
